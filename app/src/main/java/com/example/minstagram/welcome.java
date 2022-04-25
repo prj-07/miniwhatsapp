3cb1d874111faad86d2a3f6caffb24f927acc53f
prj-07 <21je0691@iitism.ac.in>
2022-04-25 22:41:16 +0530

    chat layout partly done

--- app/src/main/java/com/example/minstagram/welcome.java
+++ app/src/main/java/com/example/minstagram/welcome.java
@@ -1,22 +1,58 @@
 package com.example.minstagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.EditText;
 
-public class welcome extends AppCompatActivity {
-    EditText wel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+public class welcome extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    MyAdapter myAdapter;
+    ArrayList<ListItem>list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_welcome);
+        recyclerView = findViewById(R.id.userlist);
+        database = FirebaseDatabase.getInstance().getReference("Users");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        list = new ArrayList<>();
 
-        wel = findViewById(R.id.welcome);
-        String name= getIntent().getStringExtra("name");
-        wel.setText(name);
+        myAdapter = new MyAdapter(this,list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+           for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+               ListItem listItem = dataSnapshot.getValue(ListItem.class);
+
+               list.add(listItem);
+           }
+           myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
